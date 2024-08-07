942ea9ab0e08ad5b7e70756acf44c9a911631b0d
Winn <winnnguyen@berkeley.edu>
2024-08-07 16:16:13 -0700

    bot

---
+++ finbert_utils.py
@@ -0,0 +1,25 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+from typing import Tuple
+
+device = 'cudo:0' if torch.cuda.is_available() else 'cpu'
+
+tokenizer = AutoTokenizer.from_pretrained('ProsusAI/finbert')
+model = AutoModelForSequenceClassification.from_pretrained('ProsusAI/finbert').to(device)
+labels = ['positive', 'negative', 'neutral']
+
+def estimate_sentiment(news):
+    if news:
+        tokens = tokenizer(news, return_tensors='pt', padding=True).to(device)
+
+        result = model(tokens['input_ids'], attention_mask=tokens['attention_mask'])['logits']
+        result = torch.nn.functional.softmax(torch.sum(result, 0), dim=-1)
+        prob = labels[torch.argmax(result)]
+        sentiment = labels[torch.argmax(result)]
+        return prob, sentiment
+    else:
+        return 0, labels[-1]
+    
+if __name__ == '__main__':
+    tensor, sentiment = estimate_sentiment(['markets responded negatively to the news!', 'traders were pleasantly surprised'])
+    print(tensor, sentiment)
